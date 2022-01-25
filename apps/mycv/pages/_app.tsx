bb848c5d601fafd597796cf6267d92056ba5cc70
Jonas Fischer <do.ob@gmx.net>
2022-01-25 08:39:07 +0100

    using progress bar component

--- apps/mycv/pages/_app.tsx
+++ apps/mycv/pages/_app.tsx
@@ -1,6 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
