698c65ec1859714b67ecdc849a2c7c85870c93c6
Jonas Fischer <do.ob@gmx.net>
2022-09-01 17:00:54 +0200

    moved test for main page to the specs folder because there is an error with Nextjs and tests in the pages folder.

--- apps/mycv/specs/index.spec.tsx
+++ apps/mycv/specs/index.spec.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import {render} from '@testing-library/react';
+import {shallow} from 'enzyme';
+import toJson from 'enzyme-to-json';
 
 import {Index} from '../pages';
 
+// this test needs to stay out of the pages folder because there is an error with Nextjs
 describe('Index', () => {
   it('should render successfully', () => {
     const {baseElement} = render(<Index />);
     expect(baseElement).toBeTruthy();
+  });
+
+  it('should render correctly', () => {
+    const wrapper = shallow(<Index />);
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
