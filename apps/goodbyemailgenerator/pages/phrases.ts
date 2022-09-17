afa73d8075d738375e6df86513fb482a7c14cc0f
Jonas <do.ob@gmx.net>
2022-09-17 13:58:57 +0200

    Add files via upload
    added new file with phrases.

---
+++ apps/goodbyemailgenerator/pages/phrases.ts
@@ -0,0 +1,71 @@
+const subjects = [
+    'Adieu, Servus und Goodbye',
+    'Alles Gute und auf Wiedersehen',
+    'Der Abschied naht',
+    'Adios, liebe Kolleginnen und Kollegen',
+    'Goodbye + Good Luck bei Allem',
+    'I\'ll be (probably not) back',
+    'In Hamburg sagt man Tschüss!',
+    'Ich verabschiede mich',
+    'Ich verändere mich',
+    'Danke und auf Wiedersehen',
+    'Es geht weiter',
+    'Ich ziehe weiter',
+    'Vor mir steht ein Neuanfang',
+    'Alles Gute!',
+    'Time to say goodbye',
+    'GG EZ'
+    ];
+
+
+const salutations = [
+    'Liebe Kolleginnen und Kollegen,',
+    'Werte Mitarbeiter:innen,',
+    'Genossinen und Genossen,',
+    'Verehrte Leidensgenossen,'
+]
+
+const intros = [
+    'an Karneval haben wir gelernt: Alles hat ein Ende, nur die Wurst hat zwei. So neigt sich meine Zeit hier nun diesem einen Ende zu.',
+    'mein letzter Arbeitstag ist im vollen Gange.',
+    'nach einer schönen und ereignisreichen Zeit werde ich mich nun neuen beruflichen Herausforderungen stellen.',
+    'auch wenn ich es selber noch nicht so recht wahrhaben will: heute ist mein letzter Arbeitstag.',
+    'endlich habe ich es geschafft, heute ist mein letzter Arbeitstag!'
+];
+
+
+const animalMetaphor = [
+    'Langsam aber stetig, wie eine Landschildkröte, habe ich in diesem Unternehmen alles abgegrast.',
+    'Wie ein Koala, der seinen Eukalyptusbaum leergefressen hat, muss  ich mich nun nach neuem Grün umsehen.',
+    'Wie ein Wolf, der zur Gefahr für das Alpha-Pärchen wurde, muss ich mir nun ein neues Rudel suchen.',
+    'Wie die Gänse im Herbst Richtung Süden ziehen, so zieht es mich jetzt in andere Gefilde.',
+    'Diese Firma war für mich immer mein sicherer Horst, doch dieses Adlerküken ist jetzt flügge geworden.',
+    'Fleißig wie eine Arbeiterameise habe ich hier mein Bestes gegeben, doch für diese Ameise ist es an der Zeit sich einen neuen Hügel zu suchen.'
+];
+
+
+const sportMetaphor = [
+    'Wie Roger Federer sagte: "Es führt kein Weg an harter Arbeit vorbei. Genieße sie. Man muss die Stunden investieren, denn es gibt immer etwas, das man verbessern kann", doch für mich war das heute der Matchball.',
+    'Der Ball ist rund und ein Spiel dauert 90 Minuten, doch in dieser Firma habe ich eindeutig schon beide Nachspielzeiten und ein komplettes Elfmeterschießen mitgemacht.',
+    'Wie beim Kugelstoßen habe ich im letzten Jahr jeden Arbeitstag auf meine Schulter gehievt und so weit geworfen wie ich konnte.',
+    'Heute war für mich die letzte Runde, der Gong hat geschlagen. Ob ich gewonnen habe weiß ich nicht, aber zumindest bin ich nicht K.O. gegangen.'
+];
+
+const typicalPhrase = [
+    'Mit einem weinenden und einem lachenden Auge bleibt mir jetzt nur noch, euch alles Gute zu wünschen.',
+    'Es gab gute und schlechte Zeiten, und ich möchte keinen Tag missen, doch jetzt ist es an der Zeit sich zu verabschieden.',
+    'Man soll gehen, wenn es am schönsten ist und so bleibt mir nur auf Wiedersehen zu sagen.',
+    'Auch die schönste Zeit geht zu Ende, und so sage ich heute Tschüss!',
+    'Ich möchte mich herzlich für die gute Zusammenarbeit bedanken und wünsche Euch alles Gute für die persönliche aber auch weitere Zukunft des Unternehmens.'
+];
+
+const goodByePhrase = [
+    'Hochachtungsvoll,',
+    'Ciao,',
+    'Au revoir,',
+    'Wir sehen uns!',
+    'CU,',
+    'Tschö mit Ö,',
+    'Bis spätersilie'
+];
+
